6e185946f076785755caae89af58331d556d68df
luyiming <luyimingchn@gmail.com>
2017-10-28 11:58:47 +0800

    add painter

---
+++ src/Painter.cpp
@@ -0,0 +1,135 @@
+#include "Painter.h"
+
+void Painter::drawAllShapes(Screen *screen) {
+    for (int i = 0; i < points.size(); i++) {
+        screen->drawPixel(points[i].x, screenHeight - points[i].y, points[i].color);
+    }
+    for (int i = 0; i < lines.size(); i++) {
+        screen->drawLine(lines[i].p1.x, screenHeight - lines[i].p1.y, lines[i].p2.x, screenHeight - lines[i].p2.y, lines[i].color);
+    }
+    for (int i = 0; i < circles.size(); i++) {
+        screen->drawCircle(circles[i].p.x, screenHeight - circles[i].p.y, circles[i].radius, circles[i].color);
+    }
+    for (int i = 0; i < ellipses.size(); i++) {
+        screen->drawEllipse(ellipses[i].p.x, screenHeight - ellipses[i].p.y, ellipses[i].rx, ellipses[i].ry, ellipses[i].color);
+    }
+
+    if (inDisplay) {
+        if (displayMode == M_Point) {
+            screen->drawPixel(displayPoint.x, screenHeight - displayPoint.y, displayPoint.color);
+        }
+        if (displayMode == M_Line) {
+            screen->drawLine(displayLine.p1.x, screenHeight - displayLine.p1.y, displayLine.p2.x, screenHeight - displayLine.p2.y, displayLine.color);
+        }
+        if (displayMode == M_Circle) {
+            screen->drawCircle(displayCircle.p.x, screenHeight - displayCircle.p.y, displayCircle.radius, displayCircle.color);
+        }
+    }
+}
+
+bool Painter::handleEvent(SDL_Event e, Screen *screen) {
+    bool quit = false;
+
+    static int startX = 0, startY = 0;
+
+    if (e.type == SDL_QUIT) {
+        quit = true;
+    }
+    else if (e.type == SDL_KEYDOWN) {
+        if (e.key.keysym.sym == SDLK_q) {
+            quit = true;
+        }
+        else if (e.key.keysym.sym == SDLK_1) {
+            displayMode = M_Point;
+            inDisplay = true;
+            printf("进入画点模式\n");
+            SDL_GetMouseState(&startX, &startY);
+        }
+        else if (e.key.keysym.sym == SDLK_2) {
+            if (displayMode != M_Line)
+                inDisplay = false;
+            printf("进入画线模式\n");
+            displayMode = M_Line;
+        }
+        else if (e.key.keysym.sym == SDLK_3) {
+            if (displayMode != M_Circle)
+                inDisplay = false;
+            printf("进入画圆模式\n");
+            displayMode = M_Circle;
+        }
+        else if (e.key.keysym.sym == SDLK_c) {
+            if (displayMode != M_Point)
+                inDisplay = false;
+            points.clear();
+            lines.clear();
+            circles.clear();
+            ellipses.clear();
+        }
+    }
+    else if (e.type == SDL_MOUSEBUTTONDOWN) {
+        if (e.button.button == SDL_BUTTON_LEFT) {
+            if (inDisplay == false) {
+                inDisplay = true;
+            }
+            else if (inDisplay == true) {
+                if (displayMode == M_Point) {
+                    points.push_back(Point(e.button.x, e.button.y));
+                    printf("Point:  (%d, %d)\n", e.button.x, e.button.y);
+                }
+                else if (displayMode == M_Line) {
+                    lines.push_back(Line(Point(startX, startY), Point(e.button.x, e.button.y)));
+                    printf("Line:   (%d, %d) - (%d, %d)\n", startX, startY, e.button.x, e.button.y);
+                }
+                else if (displayMode == M_Circle) {
+                    int radius = (int)sqrt((e.motion.x - startX) * (e.motion.x - startX) + (e.motion.y - startY) * (e.motion.y - startY));
+                    circles.push_back(Circle(Point(startX, startY), radius));
+                    printf("Circle: (%d, %d) - %d\n", e.button.x, e.button.y, radius);
+                    inDisplay = false;
+                }
+            }
+            //printf("%d %d\n", e.button.x, e.button.y);
+            startX = e.button.x;
+            startY = e.button.y;
+
+            if (displayMode == M_Point) {
+                displayPoint.x = e.motion.x;
+                displayPoint.y = e.motion.y;
+            }
+            else if (displayMode == M_Line) {
+                displayLine.p1.x = startX;
+                displayLine.p1.y = startY;
+                displayLine.p2.x = e.motion.x;
+                displayLine.p2.y = e.motion.y;
+            }
+            else if (displayMode == M_Circle) {
+                int r = (int)sqrt((e.motion.x - startX) * (e.motion.x - startX) + (e.motion.y - startY) * (e.motion.y - startY));
+                displayCircle.p.x = startX;
+                displayCircle.p.y = startY;
+                displayCircle.radius = r;
+            }
+        }
+        else if (e.button.button == SDL_BUTTON_RIGHT) {
+            if (displayMode != M_Point)
+                inDisplay = false;
+        }
+    }
+    else if (e.type == SDL_MOUSEMOTION) {
+        if (displayMode == M_Point) {
+            displayPoint.x = e.motion.x;
+            displayPoint.y = e.motion.y;
+        }
+        else if (displayMode == M_Line) {
+            displayLine.p1.x = startX;
+            displayLine.p1.y = startY;
+            displayLine.p2.x = e.motion.x;
+            displayLine.p2.y = e.motion.y;
+        }
+        else if (displayMode == M_Circle) {
+            int r = (int)sqrt((e.motion.x - startX) * (e.motion.x - startX) + (e.motion.y - startY) * (e.motion.y - startY));
+            displayCircle.p.x = startX;
+            displayCircle.p.y = startY;
+            displayCircle.radius = r;
+        }
+    }
+    return quit;
+}
---
+++ src/Painter.h
@@ -0,0 +1,103 @@
+#ifndef PAINTER_H
+#define PAINTER_H
+
+#include <vector>
+#include "Screen.h"
+#include <cstdio>
+
+using namespace std;
+
+struct Point {
+    int x, y;
+    color_t color = COLOR_WHITE;
+    Point() {
+        x = y = 0;
+    }
+    Point(int x, int y, color_t color = COLOR_WHITE) {
+        this->x = x;
+        this->y = y;
+        this->color = color;
+    }
+};
+
+struct Line {
+    Point p1, p2;
+    color_t color = COLOR_WHITE;
+    Line() {
+        p1 = p2 = Point();
+    }
+    Line(Point p1, Point p2, color_t color = COLOR_WHITE) {
+        this->p1 = p1;
+        this->p2 = p2;;
+        this->color = color;
+    }
+};
+
+struct Circle {
+    Point p;
+    int radius;
+    color_t color = COLOR_WHITE;
+    Circle() {
+        p = Point();
+        radius = 0;
+    }
+    Circle(Point p, int radius, color_t color = COLOR_WHITE) {
+        this->p = p;
+        this->radius = radius;
+        this->color = color;
+    }
+};
+
+struct Ellipsed {
+    Point p;
+    int rx, ry;
+    color_t color = COLOR_WHITE;
+    Ellipsed() {
+        p = Point();
+        rx = ry = 0;
+    }
+    Ellipsed(Point p, int rx, int ry, color_t color = COLOR_WHITE) {
+        this->p = p;
+        this->rx = rx;
+        this->ry = ry;
+        this->color = color;
+    }
+};
+
+
+
+class Painter {
+private:
+    enum DisplayMode { M_None, M_Point, M_Line, M_Circle };
+    bool inDisplay = true;
+    DisplayMode displayMode = M_Point;
+    Point displayPoint;
+    Line displayLine;
+    Circle displayCircle;
+
+private:
+    vector<Point> points;
+    vector<Line> lines;
+    vector<Circle> circles;
+    vector<Ellipsed> ellipses;
+    int screenWidth = 640;
+    int screenHeight = 480;
+
+public:
+    void addPoint(Point point) { points.push_back(point); }
+    void addLine(Line line) { lines.push_back(line); }
+    void addCircle(Circle circle) { circles.push_back(circle); }
+    void addEllipse(Ellipsed ellipse) { ellipses.push_back(ellipse); }
+
+public:
+    Painter() {}
+    bool handleEvent(SDL_Event e, Screen *screen);
+    void drawAllShapes(Screen *screen);
+    void init() {
+        printf("键盘按键 1: 画点模式  2: 画线模式  3: 画圆模式  c: 清除已有图形\n");
+        printf("鼠标左键: 开始绘图  鼠标右键: 结束绘图\n\n");
+        printf("进入画点模式\n");
+    }
+};
+
+#endif // PAINTER_H
